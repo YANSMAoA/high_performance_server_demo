24c9d461a071f4d86454da1b0bea8eaad2d6cb26
Ysmmm <1850955441@qq.com>
2025-04-01 18:02:26 +0800

    :UI

--- Router.h
+++ Router.h
@@ -3,6 +3,7 @@
 #include "HttpResponse.h"
 #include "Database.h"
 #include <functional>
+#include <fstream>
 
 class Router {
 public:
@@ -21,15 +22,63 @@ public:
         return HttpResponse::makeErrorResponse(404, "Not Found");
     }
 
+    std::string readFile(const std::string& filePath) {
+        std::ifstream file(filePath);
+
+        if (!file.is_open()) {
+            return "Error: Unable to open file " + filePath;
+        }
+
+        std::stringstream buffer;
+
+        buffer << file.rdbuf();
+
+        return buffer.str();
+    }
+
     void setupDatabaseRoutes(Database& db) {
         
+        addRoute("GET", "/register", [this](const HttpRequest& req) -> HttpResponse {
+            HttpResponse response;
+            response.setStatusCode(200);
+            response.setHeader("Content-Type", "text/html");
+            response.setBody(readFile("UI/login.html"));
+            return response;
+        });
+
+        addRoute("POST", "/login", [this](const HttpRequest& req) -> HttpResponse {
+            HttpResponse response;
+            response.setStatusCode(200);
+            response.setHeader("Content-Type", "text/html");
+            response.setBody(readFile("UI/register.html"));
+            return response;
+        });
+
         addRoute("POST", "/register", [&db](const HttpRequest& req) -> HttpResponse {
             auto params = req.parseFormbody();
             std::string username = params["username"];
             std::string password = params["pasword"];
 
             if (db.registerUser(username, password)) {
-                return HttpResponse::makeOkResponse("Register Success!");
+                HttpResponse response;
+                response.setStatusCode(200); 
+                response.setHeader("Content-Type", "text/html");
+                std::string responseBody = R"(
+                    <html>
+                    <head>
+                        <title>Register Success</title>
+                        <script type="text/javascript">
+                            alert("Register Success!");
+                            window.location = "/login";
+                        </script>
+                    </head>
+                    <body>
+                        <h2>moving to login...</h2>
+                    </body>
+                    </html>
+                )";
+                response.setBody(responseBody);
+                return response;
             } else {
                 return HttpResponse::makeErrorResponse(400, "Register Failed!");
             }
@@ -40,9 +89,17 @@ public:
             std::string username = params["username"];
             std::string password = params["password"];
             if (db.loginUser(username, password)) {
-                return HttpResponse::makeOkResponse("Login Success!");
+                HttpResponse response;
+                response.setStatusCode(200); 
+                response.setHeader("Content-Type", "text/html");
+                response.setBody("<html><body><h2>Login Successful</h2></body></html>");
+                return response;
             } else {
-                return HttpResponse::makeErrorResponse(400, "Login Failed!");
+                HttpResponse response;
+                response.setStatusCode(401); 
+                response.setHeader("Content-Type", "text/html");
+                response.setBody("<html><body><h2>Login Failed</h2></body></html>");
+                return response;
             }
         });
     }
